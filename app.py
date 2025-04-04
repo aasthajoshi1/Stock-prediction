e80d418dec0a53b2f25cbd75ccb468f1796f436b
aasthajoshi1 <168579940+aasthajoshi1@users.noreply.github.com>
2025-04-04 11:36:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -136,6 +136,8 @@ def download_file(filename):
     return send_file(f"static/{filename}", as_attachment=True)
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
+    port = int(os.environ.get("PORT", 8080))  # use PORT from Render
+    app.run(debug=True, host="0.0.0.0", port=port)
+
 
 
